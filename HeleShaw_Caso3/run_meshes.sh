06a58c3fbe50fae96ae0034eca39b2e46395fd0f
BorgesMath <matheusborgessampaio@gmail.com>
2025-02-11 10:22:58 -0300

    Mudando o run_meshes

--- HeleShaw_Caso3/run_meshes.sh
+++ HeleShaw_Caso3/run_meshes.sh
@@ -47,8 +47,33 @@ while [ $iteration -le 10 ]; do
     # Calcular o produto xMesh * yMesh (opcional, se desejar usar)
     product=$(echo "$xMesh * $yMesh" | bc -l)
 
+
+
+        # ----------------------------------------------------------
+    # NOVA PARTE: Extrair a média do valor da BC "inlet" do arquivo p
+    #
+    # Procura o diretório com o último tempo (apenas pastas numéricas)
+    last_time=$(ls "$case_name" | grep -E '^[0-9]+(\.[0-9]+)?$' | sort -V | tail -n1)
+    p_file="$case_name/$last_time/p"
+    
+    if [ -f "$p_file" ]; then
+        # Procura no arquivo o bloco referente à BC "inlet" e pega a linha com "value"
+        inlet_value_line=$(grep -A 3 "inlet" "$p_file" | grep "value")
+        # Exemplo da linha obtida:
+        #   value           nonuniform List<scalar> 4(0.00110946 0.00100346 0.00100346 0.00110946);
+        
+        # Extrai os números que estão entre parênteses
+        numbers=$(echo "$inlet_value_line" | awk -F'[()]' '{print $2}')
+        
+        # Calcula a média dos números extraídos
+        inlet_avg=$(echo "$numbers" | awk '{sum=0; n=NF; for(i=1;i<=n;i++){sum+=$i} if(n>0) printf "%.8f", sum/n; else print 0}')
+    else
+        inlet_avg="NA"
+    fi
+    # ----------------------------------------------------------
+
     # Gravar os resultados no arquivo results.txt
-    echo "$case_name $flow $product" >> results.txt
+    echo "$case_name $flow $product $inlet_avg" >> results.txt
 
     # Atualizar o contador de iterações
     ((iteration++))
